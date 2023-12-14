b39462e9fc333f854a0c4d757ee797b49fae7e1f
jayshahinventyv <jayshahinventyv@gmail.com>
2023-12-14 18:51:35 +0530

    updation

--- FlowChart codes/Ex18.cpp
+++ FlowChart codes/Ex18.cpp
@@ -1,1 +1,19 @@
 // Pattern: 1 1 1 3 2 5 3 7 5 9 8 11 13 13 ...n=7
+
+// Pattern : 1 2 3 2 3 4 5 4 3 4 5 6 7 6 5 4... n=4
+
+#include <iostream>
+using namespace std;
+int main() {
+   int n,sum,x=1,j=1,i=1;
+   cin>>n;
+   for(;x<=n;x++){
+       cout<<i<<" ";
+       sum=i+j;
+       i=j;
+       j=sum;
+      cout<<2*x-1<<" ";
+   }
+
+    return 0;
+}
