8e59d2c10f0efd68e7dff2a603d2fbe0cd37b1e1
jayshahinventyv <jayshahinventyv@gmail.com>
2024-01-02 14:03:39 +0530

    changes

--- HTTP_methods_Versions_Assignment_4/v2.js
+++ HTTP_methods_Versions_Assignment_4/v2.js
@@ -1,10 +1,34 @@
-// import { Client, Request } from 'react-native-http2';
-// console.log(1)
-async function displayData2() {
-  // const client = new Client();
-  const request = new Request('http://localhost:3000/mobiles', {
-    method: 'GET',
-  })
-  const response = await client.send(request);
-  console.log(response);
+const http2 = require('http2');
+function displayData2() {
+
+  // Replace 'your-http2-endpoint.com' and '/path' with your specific endpoint and path
+  const options = {
+    hostname: "http://localhost:3000/mobiles",
+    // path: '/mobiles',
+    method: 'GET', // or 'POST', 'PUT', 'DELETE', etc.
+  };
+  
+  const req = http2.request(options, (res) => {
+    res.on('data', (chunk) => {
+      console.log(`Received chunk of data: ${chunk}`);
+      // Process received data here
+    });
+  
+    res.on('end', () => {
+      console.log('No more data received');
+      // Do something after all data has been received
+    });
+  });
+  
+  req.on('error', (e) => {
+    console.error(`Request error: ${e}`);
+  });
+  
+  // Send the request
+  req.end();
+
+
+  
 }
+
+// http.createServer(() => {console.log("Listening on 5000")}).listen(8080);
