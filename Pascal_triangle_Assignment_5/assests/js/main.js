19087348e71f77eff466fae285d2d4fd72d527e5
jayshahinventyv <jayshahinventyv@gmail.com>
2024-01-05 11:29:30 +0530

    allCal

--- Pascal_triangle_Assignment_5/assests/js/main.js
+++ Pascal_triangle_Assignment_5/assests/js/main.js
@@ -1,10 +1,10 @@
-let num=prompt("Enter the number which is atleast 4 digit:");
+let num = prompt("Enter the number which is atleast 4 digit:");
 
-const finalArr = generateNumbers(num);
-// P&C
-function generateNumbers(num) {
+
+
+const result = [];
+function genNums(num) {
     const digits = num.toString().split('');
-    const result = [];
 
     function permute(nums, path = []) {
         if (path.length > 0 && parseInt(path.join('')) !== num) {
@@ -24,67 +24,98 @@ function generateNumbers(num) {
     }
 
     permute(digits);
-    return result.filter((n, index) => result.indexOf(n) === index); // Remove duplicates if any
+    return result.filter((n, index) => result.indexOf(n) === index); 
 }
-// console.log(finalArr)
-
-// Prime from finalArr
-let primeArr=[];
-function isPrime(num) {
-    if (num <= 1) {
+console.log(result)
+// Checking Prime number
+function isPrime(number) {
+    if (number < 2)
         return false;
-    }
-    for (let i = 2; i <= Math.sqrt(num); i++) {
-        if (num % i === 0) {
+
+    for (let i = 2; i <= Math.sqrt(number); i++) {
+        if (number % i === 0) {
             return false;
         }
     }
+
     return true;
 }
 
-function storePrimes(elements) {
-    const primeNumbers = elements.filter((num) => isPrime(num));
-    return primeNumbers;
+// Generating and Printing Pascal Triangle
+function generatePascals(stopElement, primeNumbers, matched_elements) {
+    let row = [1];
+    let found = false;
+
+    const outputContainer = document.getElementById('pascaltriangle');
+    outputContainer.innerHTML = '';
+    outputContainer.innerHTML += `<span class="text-element" style="margin-right: 0px;">${row}</span><br><br>`;
+    while (!found) {
+        if (!found) {
+            const nextRow = [1];
+            outputContainer.innerHTML += `<span class="text-element">1</span> `;
+
+
+            for (let i = 1; i < row.length; i++) {
+                nextRow[i] = row[i - 1] + row[i];
+                let x = nextRow[i];
+
+                if (x >= stopElement) {
+                    found = true;
+                }
+
+                if (primeNumbers.includes(x)) {
+                    matched_elements.push(x);
+                    outputContainer.innerHTML += `<span class="text-element prime">${x}</span> `;
+                }
+                else {
+                    outputContainer.innerHTML += `<span class="text-element">${x}</span> `;
+                }
+            }
+
+            nextRow.push(1);
+            row = nextRow;
+        }
+
+        outputContainer.innerHTML += `<span style="margin-right: 0px;" class="text-element">1</span><p></p>`;
+    }
+    document.getElementById('info').innerHTML+=`The number you entered : ${num}<hr>`
 }
 
-// Storing prime numbers in a separate array
-primeArr = storePrimes(finalArr);
-console.log(primeArr);
 
 
+// Main function of calling all function
+let matched_elements = [];
+let missed_elements = [];
 
-// Pascal Triangle
-  function generateCenteredPascalsTriangle(limit, combinations) {
-    const triangle = [];
-    let triangleHTML = '';
+let arr = genNums(num);
+arr = [... new Set(arr)];
+console.log("Generated numbers:", arr);
 
-    for (let i = 0; i < limit; i++) {
-      triangle[i] = [];
-      for (let j = 0; j <= i; j++) {
-        if (j === 0 || j === i) {
-          triangle[i][j] = 1;
-        } else {
-          triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
-        }
-      }
 
-      const tabs = '\t'.repeat((limit - i - 1)); // Adjust the multiplier for tab spacing
-      const rowValues = triangle[i];
+const primeNumbers = arr.filter(isPrime);
+console.log("Prime numbers:", primeNumbers);
 
-      let centeredRow = '';
-      for (let k = 0; k < rowValues.length; k++) {
-        centeredRow += `${rowValues[k]}\t\t`;
-        if (isPrime(rowValues[k]) && combinations.includes(rowValues[k])) {
-          centeredRow = centeredRow.replace(`${rowValues[k]}\t\t`, `<span class="prime">${rowValues[k]}</span>\t\t`);
-        }
-      }
+const stopElement = Math.max(...primeNumbers);
+console.log("Largest prime number:", stopElement);
+generatePascals(stopElement, primeNumbers, matched_elements);
 
-      triangleHTML += `${tabs}${centeredRow}\n`;
+
+matched_elements = [... new Set(matched_elements)];
+console.log("Matched elements:", matched_elements);
+
+
+for (let i of primeNumbers) {
+    if (!matched_elements.includes(i)) {
+        missed_elements.push(i);
     }
+}
 
-    document.getElementById('pascalsTriangle').innerHTML = triangleHTML;
-  }
+console.log("Missed elements:", missed_elements);
+// Calculation
+document.getElementById('detail').innerHTML+=`<hr><b>Generated P&C :</b> ${result}<br/><br/>`
+document.getElementById('detail').innerHTML+=`<b>Generated Prime number from generated P&C :</b> ${primeNumbers}<br/><br/>`
+document.getElementById('detail').innerHTML+=`<b>Max Prime number :</b> ${stopElement}<br/><br/>`
+document.getElementById('detail').innerHTML+=`<b>Matched Elements:</b> ${matched_elements}<br/><br/>`
+document.getElementById('detail').innerHTML+=`<b>Missed Elements</b> ${missed_elements}<br/><br/>`
+document.getElementById('detail').innerHTML+=`<b>Error Ratio :</b> ${((missed_elements.length/primeNumbers.length) * 100).toFixed(3)} %</p><br/><br/>`;
 
-
-  const limit = 15;
-  generateCenteredPascalsTriangle(limit, primeArr);
